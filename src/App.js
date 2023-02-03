9b5ab49733050f7ae82720bd66ff42a0a791401a
ariqfrhan <110904982+ariqfrhan@users.noreply.github.com>
2023-02-03 11:34:27 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import MyButton from './components/myButton';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import React, {useState} from "react";
 
-function App() {
+const App =() => {
+  const navHeading = "Navigation Bar";
+  const navText = "Social Media";
+
+  const clicked = () =>{
+    return alert("button clicked!");
+  }
+
+  const paragraf = () =>{
+    return (
+      <div>
+        <h3>Lorem Ipsum</h3>
+        <marquee>Loreeem ipsuuum</marquee>
+        <i>Lorem Ipsum </i>
+      </div>
+    );
+  }
+
+
+  const [getNavbarValue, setNavbarValue] = useState("")
+
+  const changeNavbarValue = () =>{
+    setNavbarValue("My Contact");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
+        <Navbar navValue = {getNavbarValue} navText={navText} navHeading={navHeading}></Navbar>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Testing ReactJs
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => changeNavbarValue()}>Ubah Navigasi</button>
+        <MyButton clicked={clicked}></MyButton>
+        <Footer paragraf={paragraf}></Footer>
       </header>
     </div>
   );
